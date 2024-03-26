abc586e43aac71793a61720f3a6cc721237c6504
Daniel <daniellsoouza380@gmail.com>
2024-03-26 18:56:12 -0300

    lendo dados planilha

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var XLSX = require("xlsx");
-var arquivo = XLSX.readFile("excel/planilha.xlsx");
+var workbook = XLSX.readFile("excel/dados.xlsx");
 
+let planilha = workbook.Sheets[workbook.SheetNames[0]];
+
+
+ for (let index = 2; index < 5; index++) {
+    const id = planilha[`A${index}`].v;
+    const name = planilha[`B${index}`].v;
+
+   console.log({id:id, name:name})
+ }
