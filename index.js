723c93d7a2c7955550f15da915061e2dc1f6100b
Daniel <daniellsoouza380@gmail.com>
2024-05-27 12:23:53 -0300

    generated certificate

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const { PDFDocument, StandardFonts, rgb} = require("pdf-lib");
-const { writeFileSync } = require("fs");
+const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
+const { writeFileSync, readFileSync } = require("fs");
 
 var XLSX = require("xlsx");
 var workbook = XLSX.readFile("excel/alunos.xlsx");
 
 let planilha = workbook.Sheets[workbook.SheetNames[0]];
 
-for (let index = 2; index < 3; index++) {
+
+const numeroCertificados = 3
+
+// pegar dados planilha
+for (let index = 2; index < (numeroCertificados + 2); index++) {
   let RA = planilha[`A${index}`].v;
   let nome = planilha[`B${index}`].v;
   let curso = planilha[`C${index}`].v
@@ -14,30 +18,36 @@ for (let index = 2; index < 3; index++) {
   let cidade = planilha[`E${index}`].v
   let data = planilha[`F${index}`].v
 
-  let text = `Certificamos que o aluno: ${nome}, RA: ${RA} concluiu o curso de\n`+
-  `${curso}, com carga horária de ${duracao}, na cidade de ${cidade}, data ${data}\n`
-  
-    //createPDF(name)
+  let text = `Certificamos que o aluno: ${nome}, com registro acadêmico: ${RA} \nconcluiu o curso de ` +
+    `${curso}, com carga horária de ${duracao}, \nna cidade de ${cidade}, data ${data}\n`
 
-    console.log(text);
- }
+  createPDF(nome, text)
 
-async function createPDF(user) {
-  const PDFdoc = await PDFDocument.create();
-  const page = PDFdoc.addPage();
-  
-  const fontSize = 25
+}
+
+
+// modificar pdf, inserir dados aluno
+async function createPDF(nome, text) {
+
+  const document = await PDFDocument.load(readFileSync("./model_pdf/modelo-certificado.pdf"));
+
+  const page = document.getPage(0);
+
+  const fontSize =  17
   const { width, height } = page.getSize()
 
-  const timesRomanFont = await PDFdoc.embedFont(StandardFonts.TimesRoman)
+  const romanFont = await document.embedFont(StandardFonts.TimesRoman)
+  const boldFont = await document.embedFont(StandardFonts.TimesRomanBold)
 
-  page.drawText(`Hello my name is ${user}`, {
-  x: 50,
-  y: height - 4 * fontSize,
-  size: fontSize,
-  font: timesRomanFont,
-  color: rgb(0, 0.53, 0.71),
-})
+  page.drawText(text, {
+    x: 50,
+    y: height - 14 * fontSize,
+    size: fontSize,
+    font: romanFont,
+    color: rgb(0, 0, 0),
+  }
+)
 
-  writeFileSync(user + ".pdf", await PDFdoc.save());
+  writeFileSync(nome + ".pdf", await document.save());
 }
+
